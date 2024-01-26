e1c4ad68f64775b367a2bb1794adab2707a91e63
Romulo Vieira <romulo.vieira777@gmail.com>
2024-01-26 15:50:54 -0300

    Introdução

---
+++ Aula_01_Introducao/instalacao.r
@@ -0,0 +1,22 @@
+library(forecast)
+library(dplyr)
+
+# Atribuicao de variavel
+variavel <- sqrt(4)
+sqrt(4)
+
+# Atribuicao de variavel
+sqrt(4) -> variavel
+variavel
+
+# Remover variaveis
+rm(variavel)
+
+log(10)
+a <- 1 + 2
+a <- 2 * 2
+a <- 2/ 2
+
+sin(10)
+cos(10)
+tan(10)
